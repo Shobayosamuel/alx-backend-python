c104ad86404fa7582189d061f6ad837d88ec06b5
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-06 21:45:31 +0000

    task 3: convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function to return the string representaion of a float"""
+
+
+def to_str(n: float) -> str:
+    """Return the string repr of the float"""
+    return str(n)
