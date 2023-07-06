a82753ac8337c2d4c2c9a181e60f7363759f9c65
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-06 21:23:56 +0000

    docs: fix documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Annotated function to add two floats"""
+
+
 def add(a: float, b: float) -> float:
     """Return the sum of the floats"""
     return a+b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Annotated function to concatenate two strings"""
 
 
 def concat(str1: str, str2: str) -> str:
