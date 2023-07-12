0a55e610d7bc337dcdbf8f245e0be879f445857d
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-12 17:35:29 +0000

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,9 @@
 
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yield the random integers"""
     for i in range(10):
         await asyncio.sleep(1)
