d97ba9d03967b1ef0a1b76eef4c95722103ca6e6
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-12 18:03:42 +0000

    task 1: async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Function to return a list"""
+
+
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return the list of random numbers"""
+    result = [i async for i in async_generator()]
+    return result
