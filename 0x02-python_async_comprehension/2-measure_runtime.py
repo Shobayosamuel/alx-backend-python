481d483fa5137338e2ce90897727719b3d6a9b49
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-13 10:26:36 +0000

    fix: return type

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
         Return the time taken to run the list of async comprehension
     """
