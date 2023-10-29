30b00a716c52b39f21c1586064a72d2fba109e0f
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-10-29 19:14:49 +0000

    Parameterize a unit test using AssertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test Access nested map with key path."""
         self.assertEqual(access_nested_map(nested_map, path), result)
 
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test to raise keyError"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
