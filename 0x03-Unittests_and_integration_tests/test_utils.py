36d5c5fec08bbf284d8fc080c9f3d14fbba039d4
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-10-30 12:21:27 +0000

    Implement test for memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """class to test the access to nested map"""
 import unittest
 from unittest.mock import Mock, patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 import requests
 
@@ -27,6 +27,7 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     """_summary_
 
@@ -43,8 +44,40 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
         with patch('requests.get', return_value=mock_response):
             result = get_json(test_url)
-            #requests.get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """_summary_
+
+    Args:
+                    unittest (_type_): _description_
+    """
+    def test_memoize(self):
+        """_summary_
+
+        Returns:
+                _type_: _description_
+        """
+        class TestClass:
+            """_summary_
+            """
+            def a_method(self):
+                """Returns 42"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """__sumary__: return amethod"""
+                return self.a_method()
+        new_obj = TestClass()
+        with patch.object(new_obj, "a_method") as mock_method:
+            mock_method.return_value = 42
+            result_1 = new_obj.a_property
+            result_2 = new_obj.a_property
+            self.assertEqual(result_1, 42)
+            self.assertEqual(result_2, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
